9b83e63cfd66d1446c9bc27d80b622c796635792
Dafneesanch23 <daaphsanchez@gmail.com>
2024-01-09 13:04:55 -0600

    Se terminan los ejercicios con funciones con nombre, primer avance

---
+++ Funciones/EjercicioFunciones.js
@@ -0,0 +1,49 @@
+/**Ejercicios para esta sesion
+ * 
+ * Declara una funcion para cada problema planteado
+ * 
+ * calcular el perimetro de un circulo
+ * calcular el area de un rectangulo
+ * calcular el cuadrado de un numero
+ * calcular la conversion de grados celsius a farenheit
+ * calcular el valor del voltaje dadas la resistencia y la corriente
+ * calcular el volumen de una esfera
+ * 
+ * CONSIDERACIONES: poner en practica las mejores practicas para declarar funciones
+ * Los ejercicios se entregan tanto en funciones con nombre como funciones flecha
+ */
+
+
+// FUNCIONES CON NOMBRE
+// 1. Perímetro de un círculo
+
+function perimetroCirculo (d) {
+    console.log(Math.PI * d);
+    }
+
+// 2. Área de un rectángulo
+
+function areaRectangulo (b, h) {
+    console.log(b * h);
+    }
+
+// 3. Cuadrado de un número
+
+function cuadradoNumero (n) {
+    console.log(n * n);
+    }
+    
+//4. Conversion de grados celsius a farenheit
+function convCelsiusFarenheit (c) {
+    console.log("°F=" + ((c *(9/5)) + 32));
+    }
+
+//5. Valor del voltaje dadas la resistencia y la corriente
+function formVoltaje (i, r) {
+    console.log("Voltaje=" + (i* r));
+    }
+
+// 6. Volumen de una esfera
+function volEsfera (r) {
+    console.log("Volumen=" + ((4/3) * Math.PI * (Math.pow(r, 3))));
+    }
